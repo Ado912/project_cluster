d9f37162db907b5524117c568e51c073e4b1e08c
Ado_+ <sabdowinarah24@gmail.com>
2026-05-06 21:04:47 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -221,12 +221,22 @@ with tab_analisis:
     fig_bar_all = px.bar(mean_all, x=mean_all.index, y=mean_all.values, color=mean_all.values, color_continuous_scale="RdPu")
     fig_bar_all.update_layout(template="plotly_dark", paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", height=350, margin=dict(t=20))
     st.plotly_chart(fig_bar_all, use_container_width=True)
-    # Heatmap Korelasi
-    st.markdown("### 🔥 Hubungan Antar Produk")
-    st.write("Produk dengan korelasi tinggi (warna pink cerah) sering dibeli secara bersamaan.")
-    fig_heat = px.imshow(df[FITUR].corr(), text_auto=".2f", color_continuous_scale="RdPu", template="plotly_dark")
-    fig_heat.update_layout(paper_bgcolor="rgba(0,0,0,0)")
-    st.plotly_chart(fig_heat, use_container_width=True)
+     # 4. Hubungan Antar Produk (Heatmap + Story)
+    st.markdown("## 🔥 Hubungan Antar Produk")
+    col_h1, col_h2 = st.columns([1.5, 1])
+        with col_h1:
+   	    fig_heat = px.imshow(df[FITUR].corr(), text_auto=".2f", color_continuous_scale="RdPu", template="plotly_dark")
+    	    fig_heat.update_layout(paper_bgcolor="rgba(0,0,0,0)")
+    	    st.plotly_chart(fig_heat, use_container_width=True)
+        with col_h2:
+            st.markdown("### 💡 Fakta Menarik")
+            st.write("""
+        Jika Anda melihat kotak yang sangat cerah antara **Grocery** dan **Detergents_Paper**, itu artinya:
+    
+        *Pelanggan yang membeli banyak sembako hampir pasti juga membeli banyak sabun dan tisu.*
+        
+        Hal ini sangat logis karena Toko Kelontong biasanya menyetok kedua barang ini secara bersamaan untuk dijual kembali.
+        """)
 
 # ==========================================
 # TAB 3: KODE (NOTEBOOK FULL)
